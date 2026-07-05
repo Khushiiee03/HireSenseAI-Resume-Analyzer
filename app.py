2a536eac51c9f338c48a67144470177621f5a565
Khushi Kumari <khushik0003@gmail.com>
2026-07-05 17:31:15 +0530

    Initial project setup

---
+++ app.py
@@ -0,0 +1,18 @@
+def main():
+
+    candidate_name = "Rahul Sharma"
+
+    candidate_age = 22
+
+    experience = 1.5
+
+    shortlisted = True
+
+    print(candidate_name)
+    print(candidate_age)
+    print(experience)
+    print(shortlisted)
+
+
+if __name__ == "__main__":
+    main()
